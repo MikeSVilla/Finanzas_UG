1d6d3795fcf96fa13a4ad1f5b769e09c74861e54
MikeSVilla <134906846+MikeSVilla@users.noreply.github.com>
2024-08-21 08:50:35 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Librerias
+        
+import numpy as np
+import matrix_gen as mg
+
+# Solucion 1 
+
+matrix_generada = mg.matrix_prob(10, 1)
+    
+print(matrix_generada)
+
+# Solucion 2 
+
+matrix_generada = mg.matrix_prob(25, 10, (5, 5))
+    
+print(matrix_generada)
